537634b6feaeabbf5f8630caddcbc5388c61f3cb
ji-circle <sp202401jw@gmail.com>
2025-08-15 04:28:44 +0900

    fix: cors

--- buynow/config/settings.py
+++ buynow/config/settings.py
@@ -186,7 +186,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOWEDORIGINS = {
+CORS_ALLOWED_ORIGINS = {
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://dongjak.netlify.app",
