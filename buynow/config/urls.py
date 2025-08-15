70002c25cb2d7dd7727da3c698b0c98f219283bc
ji-circle <sp202401jw@gmail.com>
2025-08-16 05:07:14 +0900

    feat: swagger

--- buynow/config/urls.py
+++ buynow/config/urls.py
@@ -14,17 +14,37 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from accounts.urls import *
+
 # from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="BuyNow API",
+        default_version="v1",
+        description="API documentation for BuyNow project",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('v1/accounts/', include('accounts.urls')),
-    path('v1/stores/', include('stores.urls')),
+    path("admin/", admin.site.urls),
+    path("v1/accounts/", include("accounts.urls")),
+    path("v1/stores/", include("stores.urls")),
     # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-    path('v1/reservations/', include('reservations.urls')),
+    path("v1/reservations/", include("reservations.urls")),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
 ]
