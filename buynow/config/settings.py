f6a0d837bbc22247ce62c25d6b4cf0af75feb122
ji-circle <sp202401jw@gmail.com>
2025-08-16 05:52:41 +0900

    fix: 한국시간으로 설정

--- buynow/config/settings.py
+++ buynow/config/settings.py
@@ -165,9 +165,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ko-kr"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Seoul"
+USE_TZ = False  # (기존 USE_TZ = True일 수도 있음)
+
 
 USE_I18N = True
 
