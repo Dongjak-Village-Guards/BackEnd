62b139487a8a467c38b642217810235a041e8ffe
ji-circle <sp202401jw@gmail.com>
2025-08-16 05:46:42 +0900

    feat: /v1/stores/<store_id>/spaces/ StoreSpacesDetailView

--- buynow/stores/urls.py
+++ buynow/stores/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from .views import StoreListView
 from .views import NumOfSpacesView
+from .views import StoreSpacesDetailView
 
 urlpatterns = [
     path("", StoreListView.as_view(), name="store-list"),  # GET /v1/stores/
     path(
         "<int:store_id>/", NumOfSpacesView.as_view(), name="num-of-spaces"
     ),  # GET /v1/stores/<store_id>/
+    path(
+        "<int:store_id>/spaces/",
+        StoreSpacesDetailView.as_view(),
+        name="store-spaces-detail",
+    ),  # GET /v1/stores/<store_id>/spaces/
 ]
