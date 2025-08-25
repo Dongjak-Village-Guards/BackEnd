0cb74d0ad8ffec4e1dfa8c0e6bd97e10209a8d50
ji-circle <sp202401jw@gmail.com>
2025-08-25 16:05:29 +0900

    fix: cors 3000 복원

--- buynow/config/settings.py
+++ buynow/config/settings.py
@@ -177,6 +177,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
     "https://dongjak.netlify.app",
     "https://designcheck.netlify.app",
     "https://nowsale.streamlit.app",
